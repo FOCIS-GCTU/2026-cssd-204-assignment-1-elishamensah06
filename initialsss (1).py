d3f27318f374f65120baccfb999d17236c878cbb
elishamensah06 <2425400665@live.gctu.edu.gh>
2026-05-31 14:13:15 +0000

    Add files via upload

---
+++ initialsss (1).py
@@ -0,0 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Elisha Akuamoah Mensah
+# STUDENT ID:  2425400665
+# Email: elishamensah06@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Elisha Akuamoah Mensah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print initials E, A, M in large stylized block letters (12 wide x 10 high each),
+    # each followed by a 4-character period block, with 3-period separators between columns.
+
+    print()
+    print("...EAM")
+    print()
+    print("...EEEEEEEEEEEE...........AAAA...........MM........MM.......")
+    print("...EE....................AAAAAA..........MMM......MMM.......")
+    print("...EE...................AA....AA.........MMMM....MMMM.......")
+    print("...EE..................AA......AA........MM.MM..MM.MM.......")
+    print("...EEEEEEEEEE.........AAAAAAAAAAAA.......MM..MMMM..MM.......")
+    print("...EE.................AAAAAAAAAAAA.......MM..MMMM..MM.......")
+    print("...EE.................AA........AA.......MM...MM...MM.......")
+    print("...EE.................AA........AA.......MM........MM.......")
+    print("...EE...........**....AA........AA.**....MM........MM.**....")
+    print("...EEEEEEEEEEEE.**....AA........AA.**....MM........MM.**....")
+    print()
+
+
+main()
